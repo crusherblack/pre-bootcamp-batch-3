104a388f0ec06ec72115880c128e20609e036c72
fadhil <fadhildarma13@gmail.com>
2021-04-29 15:55:18 +0700

    done conditional

---
+++ Week 1/condition.js
@@ -0,0 +1,158 @@
+/* 
+pengecekan kondisi
+== membandingkan sama nilainya
+=== membandingkan nilai dan type data
+> lebih besar dari 
+< lebih kecil dari 
+<= lebih kecil dari sama dengan 
+>= lebih besar dari sama dengan 
+*/
+
+//contoh kasus lampu lalu lintas
+const lampColor = "kuning";
+
+//if itu mencek jika kondisi terpenuhi
+
+function lampuLaluLintas(color) {
+  let condition;
+  if (color === "merah") {
+    condition = "Kendaraan Berhenti";
+  } else if (color === "kuning") {
+    condition = "Kendaraan Hati-hati";
+  } else {
+    condition = "Kendaraan Berjalan";
+  }
+
+  document.getElementById("output").innerHTML = condition;
+}
+
+const nilai = 78;
+
+/* 
+A = 90
+B = 70 - 89
+C = 60 - 69
+D = 40 - 59
+E = < 40 
+*/
+
+//jika nilai lebih besar sama dengan dari 90 dan nilai lebih kecil sama dengan dari 100
+if (nilai >= 90 && nilai <= 100) {
+  console.log("Nilai anda adalah A");
+} else if (nilai >= 70 && nilai <= 89) {
+  console.log("Nilai anda adalah B");
+} else if (nilai >= 60 && nilai <= 69) {
+  console.log("Nilai anda adalah C");
+} else if (nilai >= 40 && nilai <= 59) {
+  console.log("Nilai anda adalah D");
+} else if (nilai < 40) {
+  console.log("Nilai anda adalah E");
+}
+
+/* 
+gaji pokok pns = 3.000.000
+gaji pokok honorer = 1.000.000
+tunjangan = 
+1. Golongan IV = 400.000
+2. Golongan III = 300.000
+3. Golongan Lain = 100.000 
+*/
+
+const biodataKaryawanSundus = {
+  name: "Sundus",
+  golongan: "IV",
+  status: "PNS",
+};
+
+const biodataKaryawanArtha = {
+  name: "Artha",
+  golongan: "III",
+  status: "PNS",
+};
+
+const biodataKaryawanSukma = {
+  name: "Sukma",
+  status: "Honorer",
+};
+
+function hitungGaji(data) {
+  let total = 0;
+  let gajiPokok = 0;
+  let tunjangan = 0;
+
+  if (data.status === "PNS") {
+    console.log(`${data.name} berstatus PNS`);
+    gajiPokok = 3000000;
+    if (data.golongan === "IV") {
+      tunjangan = 400000;
+    } else if (data.golongan === "III") {
+      tunjangan = 300000;
+    } else {
+      tunjangan = 100000;
+    }
+  } else {
+    console.log(`${data.name} berstatus Honorer`);
+    gajiPokok = 1000000;
+  }
+
+  total = gajiPokok + tunjangan;
+  console.log("Total Gaji " + total);
+}
+
+hitungGaji(biodataKaryawanSundus);
+hitungGaji(biodataKaryawanArtha);
+hitungGaji(biodataKaryawanSukma);
+
+const color = "merah";
+
+switch (color) {
+  case "merah": //if
+    console.log("berhenti");
+    break;
+
+  case "kuning": //else if
+    console.log("hati-hati");
+    break;
+
+  case "hijau": //else if
+    console.log("berjalan");
+    break;
+
+  default:
+    // else
+    console.log("pilih lampumu");
+    break;
+}
+
+//gunakan tenary kalau cuma ada 2 kondisi
+//pakelah untuk ngereturn 1 line code saja
+const status = color === "merah" ? "Berhenti" : "Berjalan";
+
+const status2 =
+  color === "merah"
+    ? "Berhenti"
+    : color === "kuning"
+    ? "hati-hati"
+    : color === "hijau"
+    ? "berjalan"
+    : "pilih warna";
+
+/* 
+tenary operator
+condtion 
+  ? true
+  : false 
+*/
+
+/* 
+tenary operator
+condition1
+  ? true
+  condition 2
+  ? true : 
+  condition 3
+  ? true :
+  false 
+*/
+
+console.log(status);
--- Week 1/looping.js
+++ Week 1/looping.js
@@ -1,4 +1,4 @@
-console.log("ini adalah file looping");
+/* console.log("ini adalah file looping");
 
 let temp = [];
 
@@ -6,19 +6,44 @@ for (i = 0; i < 10; i++) {
   temp.push(i + 1);
 }
 
-console.log(temp);
+document.getElementById("perulanganContent").innerHTML = temp; */
 
-document.getElementById("perulanganContent").innerHTML = temp;
+const posts = [
+  {
+    id: 1,
+    title: "Belajar Javascript",
+  },
+  {
+    id: 2,
+    title: "Belajar React",
+  },
+  {
+    id: 3,
+    title: "Belajar HTML",
+  },
+  {
+    id: 4,
+    title: "Belajar React.js",
+  },
+  {
+    id: 5,
+    title: "Belajar CSS",
+  },
+  {
+    id: 6,
+    title: "Belajar Mencintaimu",
+  },
+];
 
-function renderCard(count) {
+function renderCard() {
   let cards = [];
-  for (i = 1; i <= count; i++) {
+  for (i = 0; i < posts.length; i++) {
     cards.push(
       `<div class="card">
       <img src="https://storage.dumbways.microgen.id/microgen-lms1586588410220/course/640ag5ra2k5HTML%20&%20CSS.webp"
           alt="gambar">
       <div class="card-body">
-          <h3>Judul</h3>
+          <h3>${posts[i].title}</h3>
           <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
               industry's standard dummy text ever since the 1500s</p>
           </div>
@@ -29,4 +54,103 @@ function renderCard(count) {
   document.getElementById("contentContainer").innerHTML = cards.join(" ");
 }
 
-renderCard(10);
+renderCard();
+
+/* 
+pengecekan kondisi
+== apakah sama dengan
+> lebih besar dari 
+< lebih kecil dari 
+<= lebih kecil dari sama dengan 
+>= lebih besar dari sama dengan 
+*/
+
+const number1 = 10;
+const number2 = 10;
+
+const nama1 = "fadhil";
+const nama2 = "fadhil darma";
+
+console.log(nama1 == nama2);
+
+/* 
+while akan melakukan perulangan jika kondisi terpenuhi / true dan akan berhenti 
+jika kondisi menjadi false 
+evaluasi kondisi diawal
+*/
+
+let increament = 0;
+
+while (increament < 10) {
+  console.log("aku adalah kamu " + (increament + 1));
+  increament++;
+}
+
+/* 
+do while akan melakukan perulangan minimal 1 kali dan akan menstop perulangan jika kondisi false
+evaluasi kondisi diakhir 
+*/
+
+let inc = 0;
+
+do {
+  console.log("ini adalah do while");
+  inc++;
+} while (inc < 5);
+
+for (j = 0; j < 5; j++) {
+  console.log("ini dari for");
+}
+
+const students = [
+  {
+    name: "Artha",
+    gender: "male",
+  }, // index ke 0
+  {
+    name: "Riski",
+    gender: "male",
+  }, // index ke 1
+  {
+    name: "Sundus",
+    gender: "male",
+  }, // index ke 2
+  {
+    name: "Nurlela",
+    gender: "famale",
+  }, // index ke 3
+  {
+    name: "Anggie",
+    gender: "male",
+  }, // index ke 4
+  {
+    name: "Wahyu",
+    gender: "male",
+  }, // index ke 5
+];
+
+for (z = 0; z < students.length; z++) {
+  document.write(`
+    <h1>nama: ${students[z].name}</h1>
+    <h3>Gender: ${students[z].gender}</h3>
+  `);
+}
+
+//for of hanya menerima array
+//langsung akses object properties pada element
+//digunakan hanya untuk array of object
+for (student of students) {
+  document.write(`
+    <h1>nama: ${student.name}</h1>
+    <h3>Gender: ${student.gender}</h3>
+  `);
+}
+
+//for in hanya menerima array
+//bisa akses index dari array setiap perulangan
+for (var index in students) {
+  document.write(`
+    <h1>nama: ${students[index].name}</h1>
+    <h3>Gender: ${students[index].gender}</h3>
+  `);
+}
